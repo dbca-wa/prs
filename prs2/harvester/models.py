551cfe9853cf2b48ecc711749fe9ad50b5cd0be1
Ashley Felton <ashley@ropable.com>
2017-09-13 15:46:10 +0800

    Revert "Python 2/3: StringIO/BytesIO."
    This reverts commit a3cea9d0df195a85cd24b86255b4e4fec1106227.

--- prs2/harvester/models.py
+++ prs2/harvester/models.py
@@ -8,14 +8,12 @@ from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 import json
 import logging
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO  # Python3
 import sys
 import xmltodict
-
-if sys.version_info > (3, 0):
-    from io import BytesIO  # Python3
-else:
-    from StringIO import StringIO
-
 
 from referral.models import (
     Referral, Record, Region, ReferralType, Agency, Organisation, DopTrigger,
@@ -293,10 +291,7 @@ class EmailedReferral(models.Model):
         new_record = Record.objects.create(
             name=self.subject, referral=new_ref, order_date=datetime.today())
         file_name = 'emailed_referral_{}.html'.format(reference)
-        if sys.version_info > (3, 0):
-            new_file = File(BytesIO(self.body))
-        else:
-            new_file = File(StringIO(self.body))
+        new_file = File(StringIO(self.body))
         if create_records:
             new_record.uploaded_file.save(file_name, new_file)
             new_record.save()
@@ -309,7 +304,7 @@ class EmailedReferral(models.Model):
                 name=i.name, referral=new_ref, order_date=datetime.today())
             # Duplicate the uploaded file.
             if sys.version_info > (3, 0):
-                data = BytesIO(i.attachment.read())
+                data = StringIO(i.attachment.read().decode('utf-8'))
             else:
                 data = StringIO(i.attachment.read())
             new_file = File(data)
