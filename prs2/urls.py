ea0fa75874bca581113b0f29ada1000f98b1d218
Ashley Felton <ashley@ropable.com>
2018-07-05 11:29:52 +0800

    Added redirect view for favicon.

--- prs2/urls.py
+++ prs2/urls.py
@@ -1,6 +1,8 @@
 from django.urls import include, path
+from django.conf import settings
 from django.contrib import admin
 from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic.base import RedirectView
 from api import v1_api
 
 admin.autodiscover()
@@ -9,6 +11,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='logged_out.html'), name='logout'),
+    path('favicon.ico', RedirectView.as_view(url='{}favicon.ico'.format(settings.STATIC_URL)), name='favicon'),
 ]
 
 # PRS project URLs
