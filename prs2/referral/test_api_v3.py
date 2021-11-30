95a3bb4f8588bcbbec643b893eb74b641745897e
Ashley Felton <ashley@ropable.com>
2021-11-30 10:43:15 +0800

    Unit tests for v3 API.

---
+++ prs2/referral/test_api_v3.py
@@ -0,0 +1,46 @@
+from django.urls import reverse
+from referral.test_models import PrsTestCase
+
+
+API_MODELS = [
+    'referraltype', 'region', 'organisation', 'taskstate', 'tasktype', 'user', 'tag',
+]
+
+
+class PrsAPITest(PrsTestCase):
+
+    def test_permission_resource_list(self):
+        """Test auth and anon access permission to resource lists
+        """
+        for i in API_MODELS:
+            url = reverse('api_v3:{}_api_resource'.format(i))
+            self.client.logout()
+            response = self.client.get(url)  # Anonymous user
+            self.assertEqual(response.status_code, 200)
+            self.client.login(username='normaluser', password='pass')
+            response = self.client.get(url)
+            self.assertEqual(response.status_code, 200)
+            self.client.login(username='readonlyuser', password='pass')
+            response = self.client.get(url)
+            self.assertEqual(response.status_code, 200)
+
+    def test_permission_resource_detail(self):
+        """Test auth and anon access permission to resource details
+        """
+        for i in API_MODELS:
+            url = reverse('api_v3:{}_api_resource'.format(i))
+            self.client.login(username='normaluser', password='pass')
+            response = self.client.get(url)
+            res_list = response.json()
+            if res_list:  # Object(s) exist.
+                obj_id = res_list[0]['id']
+            url = reverse('api_v3:{}_api_resource'.format(i), kwargs={'pk': obj_id})
+            response = self.client.get(url)
+            self.assertEqual(response.status_code, 200)
+            self.client.logout()
+            self.client.login(username='readonlyuser', password='pass')
+            response = self.client.get(url)
+            self.assertEqual(response.status_code, 200)
+            self.client.logout()
+            response = self.client.get(url)  # Anonymous user
+            self.assertEqual(response.status_code, 200)
