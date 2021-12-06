b2d7b0d173a4611a5d546e7a9046ce8ad049e8c8
Ashley Felton <ashley@ropable.com>
2021-12-06 10:11:06 +0800

    Bugfix String.prototype.hashCode function (undeclared variables).

--- prs2/referral/static/js/location_create.js
+++ prs2/referral/static/js/location_create.js
@@ -9,9 +9,9 @@
 String.prototype.hashCode = function(){
     var hash = 0;
     if (this.length == 0) return hash;
-    for (i = 0; i < this.length; i++) {
-        char = this.charCodeAt(i);
-        hash = ((hash<<5)-hash)+char;
+    for (var i = 0; i < this.length; i++) {
+        var char = this.charCodeAt(i);
+        var hash = ((hash << 5) - hash) + char;
         hash = hash & hash; // Convert to 32bit integer
     }
     return Math.abs(hash);  // Always return absolute value.
