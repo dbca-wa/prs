353b485ff49f02e96961f92520c874458c12cc24
Ashley Felton <ashley@ropable.com>
2025-07-25 15:15:04 +0800

    Move user profile signal to its own script.

--- prs2/referral/apps.py
+++ prs2/referral/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class ReferralConfig(AppConfig):
     default_auto_field = "django.db.models.AutoField"
     name = "referral"
+
+    def ready(self):
+        # Import module signals.
+        from referral import signals
