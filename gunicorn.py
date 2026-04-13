415b5af0e1c372a9d1283630e47ec3f7eab7983f
Ashley Felton <ashley@ropable.com>
2026-04-13 10:02:00 +0800

    Gunicorn config: shorter timeout, set control_socket

--- gunicorn.py
+++ gunicorn.py
@@ -7,7 +7,8 @@ max_requests = 2048
 max_requests_jitter = 256
 preload_app = True
 keepalive = 5  # Keepalive timeout
-timeout = 300  # Worker timeout
+timeout = 180  # Worker timeout
 graceful_timeout = 30  # Graceful shutdown timeout
 # Disable access logging.
 accesslog = None
+control_socket = "/tmp/gunicorn.ctl"
